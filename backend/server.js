51b56e3bcb8cc75b8e1ca8bca66d64c828d2bb63
Emmanuel Martínez <jmartinezes@aeromexico.com>
2022-03-28 09:45:30 -0600

    ✨ Serving Products - Back End Routes

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const products = require('./data/products');
+
+// Crear el servidor de express
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+app.get('/api/products', (req, res) => {
+    res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find(p => p._id === req.params.id);
+    res.json(product);
+});
+
+
+// Escuchar Peticiones
+app.listen(5000, () => {
+    console.log(`Server running on port 5000`);
+});
