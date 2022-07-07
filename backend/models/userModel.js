c2f8c6cff33350bc20b1e1009ee43b4d36393378
Emmanuel Martínez <emmanuel.martinez@medtrainer.com>
2022-07-06 20:34:00 -0500

    ✨ User Authentication Endpoint

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,28 +1,33 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const UserSchema = mongoose.Schema({
     name: {
         type: String,
         required: true,
-    }, 
+    },
     email: {
         type: String,
         required: true,
         unique: true,
-    }, 
+    },
     password: {
         type: String,
         required: true,
-    }, 
+    },
     isAdmin: {
         type: Boolean,
         required: true,
         default: false,
-    }, 
+    },
 }, {
     timestamps: true,
 });
 
-const User = mongoose.model('User', UserSchema);
+UserSchema.methods.matchPassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model("User", UserSchema);
 
 export default User;
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import colors from 'colors';
-import { errorHandler, notFound } from './middlewares/errorMiddleware.js';
-import connectDB from './config/db.js';
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import { errorHandler, notFound } from "./middlewares/errorMiddleware.js";
+import connectDB from "./config/db.js";
 // import products from './data/products.js';
 
-import productRoutes from './routes/productRoutes.js';
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 // Environment Variables
 dotenv.config();
@@ -16,13 +17,17 @@ connectDB();
 // Create the server
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('API is running...');
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("API is running...");
 });
 
 // Routes
 // Products
-app.use('/api/products', productRoutes);
+app.use("/api/products", productRoutes);
+// Users
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
@@ -31,5 +36,7 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running in ${ process.env.NODE_ENV } mode on port ${PORT}`.yellow.bold);
+    console.log(
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+    );
 });
