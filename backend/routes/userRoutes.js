72495f8548fea723faa1e12837ffb2637864dc6c
Emmanuel Martínez <emmanuel.martinez@medtrainer.com>
2022-07-06 21:26:47 -0500

    ✨ Custom Authentication Middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,17 @@
 import express from "express";
-import { authUser } from "../controllers/userController.js";
+import { authUser, getUserProfile } from "../controllers/userController.js";
+import { protect } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-// @desc Fetch Auth user & get token
+// @desc Auth user & get token
 // @route POST /api/users/login
 // @access Public
 router.post("/login", authUser);
+
+// @desc Get User Profile
+// @route GET /api/users/profile
+// @access Private
+router.get("/profile", protect, getUserProfile);
 
 export default router;
